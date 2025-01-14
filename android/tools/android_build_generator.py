msft-mirror-aosp/platform.external.ltp

commit 7f4f899e2bdd86989dbedb05e4185fff3c6f6e3b

ANDROID: Add lowmem and hwasan configurations using extra_test_configs
Use extra_test_configs field to add the lowmem and hwasan configurations
and reuse the binaries in vts without duplicating them.

Bug: 277261121
Test: mma
Test: atest-dev vts_ltp_test_arm_64
Test: atest-dev vts_ltp_test_arm_64_hwasan
Change-Id: Ied5a84ea7459f8395ed3e0f6fb4a36f8cf4d2aa9
Signed-off-by: Edward Liaw <edliaw@google.com>

android/tools/android_build_generator.py
@@ -39,64 +39,28 @@ TARGET_LIST = [
     {
         "arch": "arm",
         "bitness": "64",
-        "low_mem": False,
-        "hwasan": False,
+        "extra_test_configs": ["lowmem", "hwasan", "lowmem_hwasan"],
         "targets": ["arm64"],
     },
-#    {
-#        "arch": "arm",
-#        "bitness": "64",
-#        "low_mem": True,
-#        "hwasan": False,
-#        "targets": ["arm64"],
-#    },
-#    {
-#        "arch": "arm",
-#        "bitness": "64",
-#        "low_mem": False,
-#        "hwasan": True,
-#        "targets": ["arm64"],
-#    },
-#    {
-#        "arch": "arm",
-#        "bitness": "64",
-#        "low_mem": True,
-#        "hwasan": True,
-#        "targets": ["arm64"],
-#    },
     {
         "arch": "arm",
         "bitness": "32",
-        "low_mem": False,
-        "hwasan": False,
+        "extra_test_configs": ["lowmem"],
         "targets": ["arm", "arm64"],
     },
-#    {
-#        "arch": "arm",
-#        "bitness": "32",
-#        "low_mem": True,
-#        "hwasan": False,
-#        "targets": ["arm", "arm64"],
-#    },
     {
         "arch": "riscv",
         "bitness": "64",
-        "low_mem": False,
-        "hwasan": False,
         "targets": ["riscv64"],
     },
     {
         "arch": "x86",
         "bitness": "64",
-        "low_mem": False,
-        "hwasan": False,
         "targets": ["x86_64"],
     },
     {
         "arch": "x86",
         "bitness": "32",
-        "low_mem": False,
-        "hwasan": False,
         "targets": ["x86", "x86_64"],
     },
 ]
@@ -566,28 +530,42 @@ class BuildGenerator(object):
             f.write('\n'.join(bp_result))
             f.write('\n')
 
-    def ArchString(self, arch, bitness, low_mem, hwasan):
+    def ArchString(self, arch, bitness, lowmem=False, hwasan=False):
         if bitness == '32':
             arch_string = arch
         else:
             arch_string = f'{arch}_{bitness}'
-        if low_mem:
+        if lowmem:
             arch_string += '_lowmem'
         if hwasan:
             arch_string += '_hwasan'
         return arch_string
 
-    def BuildConfigGenrule(self, arch, bitness, low_mem, hwasan, targets):
+    def BuildConfigGenrule(self, arch, bitness, targets, extra_test_configs=None):
+        extra_test_configs = extra_test_configs if extra_test_configs else []
         bp_result = []
-        arch_string = self.ArchString(arch, bitness, low_mem, hwasan)
+        arch_string = self.ArchString(arch, bitness)
 
         bp_result.append('')
         bp_result.append('genrule {')
         bp_result.append('    name: "ltp_config_%s",' % arch_string)
         bp_result.append('    out: ["vts_ltp_test_%s.xml"],' % arch_string)
         bp_result.append('    tools: ["gen_ltp_config"],')
-        bp_result.append('    cmd: "$(location gen_ltp_config) --arch %s --bitness %s --low-mem %r --hwasan %r $(out)",' % (arch, bitness, low_mem, hwasan))
+        bp_result.append('    cmd: "$(location gen_ltp_config) --arch %s --bitness %s --low-mem %r --hwasan %r $(out)",' % (arch, bitness, lowmem, hwasan))
         bp_result.append('}')
+
+        for config in extra_test_configs:
+            lowmem = 'lowmem' in config
+            hwasan = 'hwasan' in config
+            arch_string = self.ArchString(arch, bitness, lowmem, hwasan)
+
+            bp_result.append('')
+            bp_result.append('genrule {')
+            bp_result.append('    name: "ltp_config_%s",' % arch_string)
+            bp_result.append('    out: ["vts_ltp_test_%s.xml"],' % arch_string)
+            bp_result.append('    tools: ["gen_ltp_config"],')
+            bp_result.append('    cmd: "$(location gen_ltp_config) --arch %s --bitness %s --low-mem %r --hwasan %r $(out)",' % (arch, bitness, lowmem, hwasan))
+            bp_result.append('}')
         return bp_result
 
     def BuildPackageList(self):
@@ -600,15 +578,16 @@ class BuildGenerator(object):
         bp_result.append(']')
         return bp_result
 
-    def BuildLTPTestSuite(self, arch, bitness, low_mem, hwasan, targets):
+    def BuildLTPTestSuite(self, arch, bitness, targets, extra_test_configs=None):
+        extra_test_configs = extra_test_configs if extra_test_configs else []
         bp_result = []
-        arch_string = self.ArchString(arch, bitness, low_mem, hwasan)
+        arch_string = self.ArchString(arch, bitness)
 
         bp_result.append('')
         bp_result.append('sh_test {')
         bp_result.append('    name: "vts_ltp_test_%s",' % arch_string)
         bp_result.append('    src: "empty.sh",')
-        bp_result.append('    test_config_template: ":ltp_config_%s",' % arch_string)
+        bp_result.append('    test_config: ":ltp_config_%s",' % arch_string)
         bp_result.append('    test_suites: [')
         bp_result.append('        "general-tests",')
         bp_result.append('        "vts",')
@@ -624,6 +603,15 @@ class BuildGenerator(object):
             bp_result.append('            enabled: true,')
             bp_result.append('        },')
         bp_result.append('    },')
+
+        if extra_test_configs:
+            bp_result.append('    extra_test_configs: [')
+            for config in extra_test_configs:
+                lowmem = 'lowmem' in config
+                hwasan = 'hwasan' in config
+                arch_string = self.ArchString(arch, bitness, lowmem, hwasan)
+                bp_result.append('        ":ltp_config_%s",' % arch_string)
+            bp_result.append('    ],')
 
         bp_result.append('    data: LTP_TESTS,')
         bp_result.append('}')

android/Android.bp
@@ -2747,10 +2747,38 @@ genrule {
 }
 
 genrule {
+    name: "ltp_config_arm_64_lowmem",
+    out: ["vts_ltp_test_arm_64_lowmem.xml"],
+    tools: ["gen_ltp_config"],
+    cmd: "$(location gen_ltp_config) --arch arm --bitness 64 --low-mem True --hwasan False $(out)",
+}
+
+genrule {
+    name: "ltp_config_arm_64_hwasan",
+    out: ["vts_ltp_test_arm_64_hwasan.xml"],
+    tools: ["gen_ltp_config"],
+    cmd: "$(location gen_ltp_config) --arch arm --bitness 64 --low-mem False --hwasan True $(out)",
+}
+
+genrule {
+    name: "ltp_config_arm_64_lowmem_hwasan",
+    out: ["vts_ltp_test_arm_64_lowmem_hwasan.xml"],
+    tools: ["gen_ltp_config"],
+    cmd: "$(location gen_ltp_config) --arch arm --bitness 64 --low-mem True --hwasan True $(out)",
+}
+
+genrule {
     name: "ltp_config_arm",
     out: ["vts_ltp_test_arm.xml"],
     tools: ["gen_ltp_config"],
     cmd: "$(location gen_ltp_config) --arch arm --bitness 32 --low-mem False --hwasan False $(out)",
+}
+
+genrule {
+    name: "ltp_config_arm_lowmem",
+    out: ["vts_ltp_test_arm_lowmem.xml"],
+    tools: ["gen_ltp_config"],
+    cmd: "$(location gen_ltp_config) --arch arm --bitness 32 --low-mem True --hwasan False $(out)",
 }
 
 genrule {
@@ -2777,7 +2805,7 @@ genrule {
 sh_test {
     name: "vts_ltp_test_arm_64",
     src: "empty.sh",
-    test_config_template: ":ltp_config_arm_64",
+    test_config: ":ltp_config_arm_64",
     test_suites: [
         "general-tests",
         "vts",
@@ -2788,13 +2816,18 @@ sh_test {
             enabled: true,
         },
     },
+    extra_test_configs: [
+        ":ltp_config_arm_64_lowmem",
+        ":ltp_config_arm_64_hwasan",
+        ":ltp_config_arm_64_lowmem_hwasan",
+    ],
     data: LTP_TESTS,
 }
 
 sh_test {
     name: "vts_ltp_test_arm",
     src: "empty.sh",
-    test_config_template: ":ltp_config_arm",
+    test_config: ":ltp_config_arm",
     test_suites: [
         "general-tests",
         "vts",
@@ -2809,13 +2842,16 @@ sh_test {
             enabled: true,
         },
     },
+    extra_test_configs: [
+        ":ltp_config_arm_lowmem",
+    ],
     data: LTP_TESTS,
 }
 
 sh_test {
     name: "vts_ltp_test_riscv_64",
     src: "empty.sh",
-    test_config_template: ":ltp_config_riscv_64",
+    test_config: ":ltp_config_riscv_64",
     test_suites: [
         "general-tests",
         "vts",
@@ -2832,7 +2868,7 @@ sh_test {
 sh_test {
     name: "vts_ltp_test_x86_64",
     src: "empty.sh",
-    test_config_template: ":ltp_config_x86_64",
+    test_config: ":ltp_config_x86_64",
     test_suites: [
         "general-tests",
         "vts",
@@ -2849,7 +2885,7 @@ sh_test {
 sh_test {
     name: "vts_ltp_test_x86",
     src: "empty.sh",
-    test_config_template: ":ltp_config_x86",
+    test_config: ":ltp_config_x86",
     test_suites: [
         "general-tests",
         "vts",

commit d75d504dae7fa4ab70b59dbd786bfeda7425470b

ANDROID: remove undefined variables
Remove some undefined variables that were left over from previous patch.

Bug: 383576395
Test: android/tools/gen_android_build.sh
Fixes: 7f4f899e2bdd ("ANDROID: Add lowmem and hwasan configurations using extra_test_configs")
Change-Id: I82ae787199fdc1a8f92dd4261e67c9de0c1a4c36
Signed-off-by: Edward Liaw <edliaw@google.com>

android/tools/android_build_generator.py
@@ -551,7 +551,7 @@ class BuildGenerator(object):
         bp_result.append('    name: "ltp_config_%s",' % arch_string)
         bp_result.append('    out: ["vts_ltp_test_%s.xml"],' % arch_string)
         bp_result.append('    tools: ["gen_ltp_config"],')
-        bp_result.append('    cmd: "$(location gen_ltp_config) --arch %s --bitness %s --low-mem %r --hwasan %r $(out)",' % (arch, bitness, lowmem, hwasan))
+        bp_result.append('    cmd: "$(location gen_ltp_config) --arch %s --bitness %s $(out)",' % (arch, bitness))
         bp_result.append('}')
 
         for config in extra_test_configs: